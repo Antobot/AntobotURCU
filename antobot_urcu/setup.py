56dbdc9dcee7a029dfccbc8b502af19befc7b9d8
Daniel Freer <daniel.freer@antobot.ai>
2025-06-04 09:31:35 +0800

    Adding setup.py

---
+++ antobot_urcu/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['antobot_urcu'],
+    package_dir={'': 'src'}
+)
+
+setup(**d)
